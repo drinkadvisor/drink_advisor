3933bc663ecb31301a2e6fda54dacc9f85b3513a
seanlanders92 <seanlanders92@gmail.com>
2020-01-27 15:39:40 -0800

    added beer and wine constructor functions

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,51 @@
+'use strict';
+
 console.log('Hello World!');
+
+Addbeer.beerDrink = [];
+Addwine.wineDrink = [];
+
+function Addbeer(name, abv, type, writtenNotes, score) {
+  this.name = name;
+  this.abv = abv;
+  this.type = type;
+  this.writtenNotes = writtenNotes;
+  this.score = score;
+
+  Addbeer.beerDrink.push(this);
+}
+
+function Addwine(name, abv, type, writtenNotes, score) {
+  this.name = name;
+  this.abv = abv;
+  this.type = type;
+  this.writtenNotes = writtenNotes;
+  this.score = score;
+
+  Addwine.wineDrink.push(this);
+}
+
+function updateStorageWine() {
+  var arrayString = JSON.stringify(Addwine.wineDrink);
+  localStorage.setItem('wineData', arrayString);
+}
+
+function updateStorageBeer() {
+  var arrayString = JSON.stringify(Addbeer.beerDrink);
+  localStorage.setItem('beertData', arrayString);
+
+}
+
+function getStorageWine(){
+  var productData = localStorage.getItem('productData');
+  var parsedData = JSON.parse(productData);
+}
+
+function getStorageBeer(){
+  var productData = localStorage.getItem('productData');
+  var parsedData = JSON.parse(productData);
+}
+
+
+
+
